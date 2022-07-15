4511872bb07fd8a9c2865806f3bdac76db6a3921
JadenFicklin <jadendficklin@gmail.com>
2022-07-14 19:41:45 -0600

    numbers will display, clear functionality

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -1,30 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Calc() {
+  const [holdNum, setHoldNum] = useState("");
+
+  const addNumber = (num) => {
+    setHoldNum(`${holdNum + num} `);
+  };
+
+  const clearCalc = () => {
+    setHoldNum(``);
+  };
+
   return (
     <>
       <div className="outer">
         <div className="inner">
-          <div className="top"></div>
+          <div className="top">{holdNum}</div>
           <div className="bottom">
             <div className="button percentage">%</div>
-            <div className="button clear">c</div>
+            <div className="button clear" onClick={() => clearCalc()}>
+              c
+            </div>
             <div className="button delete-one">X</div>
             <div className="button devide">/</div>
-            <div className="button seven">7</div>
-            <div className="button eight">8</div>
-            <div className="button nine">9</div>
+            <div className="button seven" onClick={() => addNumber(7)}>
+              7
+            </div>
+            <div className="button eight" onClick={() => addNumber(8)}>
+              8
+            </div>
+            <div className="button nine" onClick={() => addNumber(9)}>
+              9
+            </div>
             <div className="button multiply">x</div>
-            <div className="button four">4</div>
-            <div className="button five">5</div>
-            <div className="button six">6</div>
+            <div className="button four" onClick={() => addNumber(4)}>
+              4
+            </div>
+            <div className="button five" onClick={() => addNumber(5)}>
+              5
+            </div>
+            <div className="button six" onClick={() => addNumber(6)}>
+              6
+            </div>
             <div className="button subtract">-</div>
-            <div className="button one">1</div>
-            <div className="button two">2</div>
-            <div className="button three">3</div>
+            <div className="button one" onClick={() => addNumber(1)}>
+              1
+            </div>
+            <div className="button two" onClick={() => addNumber(2)}>
+              2
+            </div>
+            <div className="button three" onClick={() => addNumber(3)}>
+              3
+            </div>
             <div className="button plus">+</div>
             <div className="button empty"></div>
-            <div className="button zero">0</div>
+            <div className="button zero" onClick={() => addNumber(0)}>
+              0
+            </div>
             <div className="button period">.</div>
             <div className="button equal">=</div>
           </div>
